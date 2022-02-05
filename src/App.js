f03daee42f8edac264c7fb429dd22f656ba97b8a
bob <bobxu1989@gmail.com>
2022-02-05 10:15:32 -0600

    1st update

--- src/App.js
+++ src/App.js
@@ -2,21 +2,62 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const Square = (props) => {
+    return(
+      <button className ="square">
+        {props.value}
+      </button>
+    )
+  }
+
+  const Board = () => {
+    function renderSquare(i){
+      return <Square value={i} />;
+    }
+
+    const status = 'Next player: X';
+
+    return(
+      <div>
+        <div className='status'>{status}</div>
+        <div className='board-row'>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+
+  const Game = () => {
+    return(
+      <div className='gmae'>
+        <div className='game-board'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Game />
       </header>
     </div>
   );
