9bee8bc76962c399a6e6e5d6347dd0a3e5d808ae
bob <bobxu1989@gmail.com>
2022-02-07 23:52:59 -0600

    feat/handleClick

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
 
   const Square = (props) => {
+
     return(
-      <button className ="square">
+      <button className ="square" onClick={() => props.onClick('X')}>
         {props.value}
       </button>
     )
   }
 
-  const Board = () => {
-    function renderSquare(i){
-      return <Square value={i} />;
+  const Board = (props) => {
+
+    const [squares, setSquare] = useState(Array(9).fill(null));
+
+    const handleClick = (i) => {
+
+      const Squares = squares.slice();
+      Squares[i] = 'X';
+      setSquare(Squares);
+    }
+
+    const renderSquare = (i) => {
+      return (
+      <Square value={squares[i]} 
+       onClick={() => handleClick(i)}
+      />);
     }
 
     const status = 'Next player: X';
